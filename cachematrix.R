a84005640c8a0a5515f3078551dbb2da0bac9f70
Shuen-Ing Tschen <shuening@gmail.com>
2014-04-26 23:07:10 -0700

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special "matrix" 
+## object that can cache its inverse.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ##return a matrix argument "x"
+        i <- NULL                           ##if the cache is NULL
+        set <- function(y) {                ## replace argument x with  
+                x <<- y                     ## argument y for a new function
+                i <<- NULL
+        }
+        get <- function() x                 ## get the function with x
+        setinverse <- function(inverse) i <<- inverse ##setinverse
+        getinverse <- function() i                    ##getinverse
+        list(set= set, get = get,                     ##it returns a list with 4 items
+             setinverse = setinverse,
+             getinverse = getinverse
+             )
 }
+## cacheSolve: This function computes the inverse of the 
+## special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix 
+## has not changed), then the cachesolve should retrieve the 
+## inverse from the cache.
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {  ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()       ##query the x vector's cache
+        if(!is.null(i)){          ##if the cache exists 
+                message("getting cached data")  
+                return(i)         ## return cache
+        }
+        data <- x$get()           ##if no cache exists
+        i <- solve(data, ...)     ##compute the in inverse of the matrix here
+        x$setinverse(i)           ##save the result in the x's cache
+        i                         ##and return the result
 }
